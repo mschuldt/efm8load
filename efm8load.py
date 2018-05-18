df346f56cb4c193a72a0a12820baf1e06f29f7f5
mschuldt <mbschuldt@gmail.com>
2018-05-17 19:14:48 -0700

    fix indentation

--- efm8load.py
+++ efm8load.py
@@ -53,23 +53,23 @@ class EFM8Loader:
     """A python implementation of the EFM8 bootloader protocol"""
 
     devicelist = {
-                    # DEVICE_ID : [ NAME, { DICT OF VARIANT_IDS } ]
-                    #            VARIANT_ID: VARIANT_ID, VARIANT_NAME, FLASH_SIZE, PAGE_SIZE, SECURITY_PAGE_SIZE]
-                     0x16 : ["EFM8SB2", { } ],
-                     0x30 : ["EFM8BB1", {
-                                         0x01: ["EFM8BB10F8G_QSOP24", 8*1024, 512, 512 ],
-                                         0x02: ["EFM8BB10F8G_QFN20" , 8*1024, 512, 512 ],
-                                         0x03: ["EFM8BB10F8G_SOIC16", 8*1024, 512, 512 ],
-                                         0x05: ["EFM8BB10F4G_QFN20" , 4*1024, 512, 512 ],
-                                         0x08: ["EFM8BB10F2G_QFN20" , 2*1024, 512, 512 ],
-                                         0x12: ["EFM8BB10F8I_QFN20" , 8*1024, 512, 521 ]
-                                         }],
-                      0x32 : ["EFM8BB2", {
-                                         0x01: ["EFM8BB22F16G_QFN28" , 16*1024, 512, 512],
-                                         0x02: ["EFM8BB21F16G_QSOP24", 16*1024, 512, 512],
-                                         0x03: ["EFM8BB21F16G_QFN20" , 16*1024, 512, 512]
-                                         }]
-                 }
+        # DEVICE_ID : [ NAME, { DICT OF VARIANT_IDS } ]
+        #            VARIANT_ID: VARIANT_ID, VARIANT_NAME, FLASH_SIZE, PAGE_SIZE, SECURITY_PAGE_SIZE]
+        0x16 : ["EFM8SB2", { } ],
+        0x30 : ["EFM8BB1", {
+            0x01: ["EFM8BB10F8G_QSOP24", 8*1024, 512, 512 ],
+            0x02: ["EFM8BB10F8G_QFN20" , 8*1024, 512, 512 ],
+            0x03: ["EFM8BB10F8G_SOIC16", 8*1024, 512, 512 ],
+            0x05: ["EFM8BB10F4G_QFN20" , 4*1024, 512, 512 ],
+            0x08: ["EFM8BB10F2G_QFN20" , 2*1024, 512, 512 ],
+            0x12: ["EFM8BB10F8I_QFN20" , 8*1024, 512, 521 ]
+        }],
+        0x32 : ["EFM8BB2", {
+            0x01: ["EFM8BB22F16G_QFN28" , 16*1024, 512, 512],
+            0x02: ["EFM8BB21F16G_QSOP24", 16*1024, 512, 512],
+            0x03: ["EFM8BB21F16G_QFN20" , 16*1024, 512, 512]
+        }]
+    }
 
     def __init__(self, port, baud, debug = False):
         self.debug           = debug
